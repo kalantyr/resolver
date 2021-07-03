9e6a857f29f4059e7a4cc0e683463ec8e77c27cc
kalavarda <kulerda.kalavarda@gmail.com>
2021-07-03 21:48:40 +0300

    UI

--- src/resolver/controllers/MathProblemController.java
+++ src/resolver/controllers/MathProblemController.java
@@ -1,11 +1,20 @@
 package resolver.controllers;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import resolver.Main;
 import resolver.models.MathProblem;
 import resolver.models.MathProblemInfo;
 
+import java.util.Map;
+
 public class MathProblemController {
+    @FXML
+    private javafx.scene.control.TextArea inputText;
+    @FXML
+    private javafx.scene.control.TextArea resultText;
+
     private MathProblem mathProblem;
 
     public void init(Stage stage, Class<MathProblem> cl) {
@@ -15,10 +24,30 @@ public class MathProblemController {
         try
         {
             mathProblem = cl.newInstance();
-            mathProblem.equals(null);
         }
         catch (Exception e) {
             Main.showError(e);
+        }
+    }
+
+    public static Map<String,Object> parse(String text){
+        throw new RuntimeException("Не реализовано");
+    }
+
+    public static String toString(Map<String,Object> data){
+        throw new RuntimeException("Не реализовано");
+    }
+
+    @FXML
+    public void onCalculate(Event e) {
+        try
+        {
+            var inputData = parse(inputText.getText());
+            var result = mathProblem.calculate(inputData);
+            resultText.setText(toString(result));
+        }
+        catch (Exception error) {
+            Main.showError(error);
         }
     }
 }
