81c9f6e341fa21222b301861121c480bf9565e07
YuraKProgrammer <yura.kalantyr@mail.ru>
2021-07-07 11:57:26 +0300

    Добавлены квадратные уравнения и юнит тесты 2.

---
+++ src/resolver/models/KvUr.java
@@ -0,0 +1,45 @@
+package resolver.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@MathProblemInfo(name = "Квадратное уравнение", description = "Расчитывает корни квадратного уравнения")
+public class KvUr extends MathProblem {
+    @Override
+    public Map<String,Object> calculate(Map<String,Object> inputData) {
+        var result = new HashMap<String,Object>();
+
+        var a = inputData.get("a");
+        if (a==null)
+            a=0;
+        var a1=(int)a;
+
+        var b = inputData.get("b");
+        if (b==null)
+            b=0;
+        var b1=(int)b;
+
+        var c = inputData.get("c");
+        if (c==null)
+            c=0;
+        var c1=(int)c;
+
+        var d = b1*b1-4*a1*c1;
+        var k=0;
+        if (d==0)
+            k=1;
+        if (d>0)
+            k=2;
+
+        if (k>0) {
+            var x1 = (-b1 + Math.sqrt(d)) / (2 * a1);
+            result.put("x1",x1);
+            result.put("x2",x1);
+        }
+        if (k==2){
+            var x2 = (-b1 - Math.sqrt(d)) / (2 * a1);
+            result.replace("x2",x2);
+        }
+        return result;
+    }
+}
